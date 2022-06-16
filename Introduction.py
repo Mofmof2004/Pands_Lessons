91bddef8a953b3ff53afc24fdc568d161f9eb7ca
Theophile <theophile.guillet5@gmail.com>
2022-06-16 19:48:24 +0400

    First Commit

---
+++ Introduction.py
@@ -0,0 +1,16 @@
+# How to run jupiter lab
+# Just write JJupyter lab in cammand line
+# Go for notebook
+import pandas as pd
+
+# Df = dataframe
+# Dataframe is the object that is your columns and rows
+# Series is a single column
+# Panel is multiple data frames = rows, columns and depth
+
+#Read CSV + file name
+df = pd.read_csv("Panda/avocado.csv")
+
+# Print first few rows
+#df.head()
+
